2aad1af0295f74eec63865b253cab93a83eb6703
Sameer Pokhrel <sameerpokhrel2002@gmail.com>
2023-11-06 08:03:47 +0545

    done new projects like counter

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>coffee aur react | sameer prasad pokhrel</h1>
+  );
+}
+
+export default App;
---
+++ counter project/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState,useEffect} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [value, setValue] = useState(5);
+
+
+  function addValue(){
+    if(value<20){
+      value++;
+    setValue(value);
+    }
+    
+   
+  }
+  function subValue(){
+    if(value>0){
+      value--;
+    setValue(value);
+    }
+    
+  }
+
+  return (
+    <>
+     <h1>chai aur react</h1> 
+     <h2>counter value : {value}</h2>
+     <button
+      style={{marginRight:'6px'}}
+      onClick={addValue}
+      >add value</button>
+     <button onClick={subValue}>decrease value</button>
+        
+    </>
+  )
+}
+
+export default App
